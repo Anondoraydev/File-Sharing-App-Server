4be65acf89ebe3a2973c80d5ff5e4e0dd53137f1
Anondo Ray <anondo554@gmail.com>
2025-12-23 23:06:20 +0600

    config express middelware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
 import express from "express";
+import helmet from "helmet";
 const app = express();
+import cors from "cors";
+import { config } from "./config/config.ts";
+import cookieParser from "cookie-parser";
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: config.APP_URL,
+    credentials: true,
+    maxAge: 86400,
+  })
+);
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 export default app;
