a9dff7de9ea9951a10e2e51e2a6a1785a2aa739a
Anondo Ray <anondo554@gmail.com>
2025-12-24 13:39:44 +0600

    bug fixt

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,21 @@
-import mongoose from "mongoose";
 
-export const connectDB = async () => {
+import mongoose from "mongoose";
+import { config } from "../config/config.ts";
+
+export async function connectDB() {
   try {
-    await mongoose.connect(process.env.DATABASE_URL || "");
-    console.log("Connected to MongoDB");
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose Connected to MongoDB");
+    });
+
+    mongoose.connection.on("error", (error) => {
+      console.error("❌ Mongoose connection error", error);
+      process.exit(1);
+    });
+
+    await mongoose.connect(config.DATABASE_URL);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("❌ MongoDB initial connection failed", error);
+    process.exit(1);
   }
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import mongoose from "mongoose";
 import app from "./app.ts";
-import { config } from "./config/config.ts";
 import { connectDB } from "./db/index.ts";
+import { config } from "./config/config.ts";
+const server = async () => {
+  app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`);
+  });
+  await connectDB();
+};
 
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
-  connectDB();
-});
-   
+(async () => {
+  await server();
+})();
