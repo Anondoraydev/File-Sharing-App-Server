92f80d6addb7c1d9b286e767cc39d68090374b5c
Anondo Ray <anondo554@gmail.com>
2026-01-09 23:37:47 +0600

    fix(auth.route): add missing newline for code readability

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -7,6 +7,7 @@ const router: Router = express.Router();
 router.post("/auth/register", AuthController.register);
 router.post("/auth/login", AuthController.login);
 router.post("/send-otp", otpController.sendOTP);
+
 router.post("/verify-otp", otpController.verifyOTP);
 
 export default router;
