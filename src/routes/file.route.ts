d4d01daf8f0760fd06918cffd6f66a7cbdc4d0a9
Anondo Ray <anondo554@gmail.com>
2026-01-12 13:44:36 +0600

    project done

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -1,10 +1,17 @@
 import express, { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.ts";
-import { fileUpload, getFileInfo } from "../controllers/file.controller.ts";
+import {
+  downloadFile,
+  fileUpload,
+  getFileInfo,
+} from "../controllers/file.controller.ts";
 
 const router: Router = express.Router();
 
 router.post("/files", upload.single("file"), fileUpload);
+
 router.get("/files/:uuid", getFileInfo);
+
+router.post("/files/download/:uuid", downloadFile);
 
 export default router;
