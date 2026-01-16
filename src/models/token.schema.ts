f73f4e1ef8dcd873a8a389b96028ed206453df0a
Anondo Ray <anondo554@gmail.com>
2026-01-16 11:28:42 +0600

    optimize code

--- src/models/token.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const tokenSchema = new Schema({
-  token: { type: String, required: true, unique: true },
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  expiresAt: { type: Date, required: true },
-});
-
-export const TokenModel = model("Token", tokenSchema);
